e6d7ff4e5f6e280eec922c26e9f8acd14458965a
Oliver Steele <steele@osteele.com>
2021-07-24 18:03:49 +0800

    Move testdata -> examples

---
+++ testdata/free-variables.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 20);
+}
+
+function myfn(p1, p2) {
+    let l1 = gf1() + lf1() + gv1 + p1;
+    let l2 = p2 + l3 + gv2();
+    function lf1(p3) {
+        let l3 = 0;
+        let l4 = 0;
+        return p3 + l1 + gv2 + gf2() + l4;
+    }
+}
+
+function gv2() {
+}

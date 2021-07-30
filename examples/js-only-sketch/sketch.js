15c1874204bf74e7665e3164d2777f9a24fc7a39
Oliver Steele <steele@osteele.com>
2021-07-30 09:36:01 +0800

    Add descriptions to the example sketches

--- examples/js-only-sketch/sketch.js
+++ examples/js-only-sketch/sketch.js
@@ -1,3 +1,5 @@
+// Description: A directory with only a sketch.js file is itself a valid sketch.
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     text('Subdirectory with a bare JavaScript file (no index.html)', 10, 10);

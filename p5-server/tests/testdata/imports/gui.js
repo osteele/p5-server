813b4624b6ad90bb49af4d4c55b67bf74ef493e4
Oliver Steele <steele@osteele.com>
2021-10-31 13:22:50 +0800

    fix error in test case

--- p5-server/tests/testdata/imports/gui.js
+++ p5-server/tests/testdata/imports/gui.js
@@ -1,10 +1,11 @@
 let myNumber = 100;
-let myColor = color(255, 0, 0);
+let myColor;
 let myChoice = ['one', 'two', 'three'];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  myColor = color(255, 0, 0);
   var gui = createGui('My awesome GUI');
   gui.addGlobals('myColor', 'myNumber', 'myChoice');
 }
